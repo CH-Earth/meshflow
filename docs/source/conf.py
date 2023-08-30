43c53a6d3f47c89c67e522a724e0638d8c2e0606
Kasra Farmer <kasra.keshavarz@usask.ca>
2023-08-30 11:40:48 -0400

    Adding the readthedocs theme extention to the conf.py file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,11 +14,12 @@ release = '0.1.0-dev0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_rtd_theme',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
-
 
 
 # -- Options for HTML output -------------------------------------------------
